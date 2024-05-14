eb7e355232ef733777d4bc61de4ea9b2be3613c6
ipi19 <ipyanamwakikunga@gmail.com>
2024-05-14 18:30:01 +0200

    show a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/UseGenre";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, IsLoading, error } = useGenres();
+  if (IsLoading) return <Spinner />;
+  if (error) return null;
   return (
     <List>
       {data.map((genre) => (
