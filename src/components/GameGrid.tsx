c424ab80c6b8173625337b0e7ba8229906ce68bb
ipi19 <ipyanamwakikunga@gmail.com>
2024-05-14 23:51:35 +0200

    giii

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCards from "./GameCards";
 import GameCardSkelton from "./GameCardSkelton";
 import GameCardContainer from "./GameCardContainer";
@@ -7,10 +7,11 @@ import { Genre } from "../hooks/UseGenre";
 
 interface Props {
   selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-  const { data, error, IsLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+  const { data, error, IsLoading } = useGames(selectedGenre, selectedPlatform);
   const skeltons = [1, 2, 3, 4, 5, 6];
   return (
     <>
