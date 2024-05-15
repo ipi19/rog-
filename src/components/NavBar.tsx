0a241ced973b20521fd2b68aa6861105df5dc09e
ipi19 <ipyanamwakikunga@gmail.com>
2024-05-15 16:21:37 +0200

    seraching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding={"20px"}>
       <Image boxSize="65px" src={logo} />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
 
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  if (ref.current) onSearch(ref.current.value);
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={22} placeholder="serch games " variant="filled" />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={22}
+          placeholder="serch games "
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
